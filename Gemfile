723e713627d354f3963aa4f19fbc9ed0c424102d
Diego Collares <diego.collares@gmail.com>
2024-06-03 12:05:16 -0300

    create link

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@
 source "https://rubygems.org"
 
 gem "sinatra", "~> 4.0"
-gem "sequel"
+gem 'sequel', '~> 5.81'
+gem 'alphadecimal', '~> 1.1', '>= 1.1.2'
--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
-require 'sinatra'
-require 'sequel'
+%w(sinatra sequel alphadecimal).each { |gem| require gem }
 
 set :root,  File.dirname(__FILE__)
 set :views, Proc.new { File.join(root, 'app', 'views') }
@@ -13,6 +12,16 @@ DB.create_table? :urls do
   String :url
 end
 
+class Url < Sequel::Model
+  def shorten
+    self.id.alphadecimal
+  end
+
+  def self.find_shorten(shorten)
+    find(id: shorten.alphadecimal)
+  end
+end
+
 get '/' do
   erb :index
 end
@@ -21,13 +30,25 @@ post '/shorten' do
   url = params['url']
   if url_valida?(url)
     puts "Valida #{url}"
-    @shortened = 'Valid'
+    @shortened = Url.find_or_create(url: url).shorten
     erb :shortened
   else
     puts "Error #{url}"
     @error_message = 'URL inválida'
     erb :error
   end
+end
+
+get '/:id' do
+  @id = params['id']
+  @url = Url.find_shorten(@id)
+  if @id
+    redirect @url.url
+  else
+    @error_message = 'URL não encontrada'
+    erb :error
+  end
+
 end
 
 def url_valida?(url)
